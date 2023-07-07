e0b5c80421ff61c1e587ad44eedb499267ab969f
Andrew Tang <disandrewed@gmail.com>
2023-07-07 12:23:06 +1000

    cses - Subarray_Sums_II.cpp

---
+++ cses/Subarray_Sums_II.cpp
@@ -0,0 +1,74 @@
+//#include <bits/stdc++.h>
+#include <cmath>
+#include <cstring>
+#include <algorithm>
+#include <deque>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+ 
+using namespace std;
+ 
+#define LL long long
+#define MAXLL 9223372036854775806
+#define MAXN 200005
+#define MOD 1000000007
+ 
+// const LL INF = 2000000000;
+ 
+#define TESTCASE 0
+#if TESTCASE == 1
+    #define TAKE_INPUT LL t; cin >> t; while (t--) { solve(); }
+#else 
+    #define TAKE_INPUT solve();
+#endif
+
+void solve();
+int main(void) {
+    cin.tie(nullptr);
+    cin.sync_with_stdio(false);
+    TAKE_INPUT;
+}
+ 
+// CODE :D
+
+vector<LL> a;
+unordered_map<LL, LL> sums;
+
+void solve() {
+    LL n, x;
+    cin >> n >> x;
+    LL input;
+    for (LL i = 0; i < n; i++) {
+        cin >> input;
+        a.push_back(input);
+    }
+    //sort(a.begin(), a.end());
+
+    LL ans = 0;
+    
+    LL pref = 0;
+    sums[0] = 1;
+    for (LL i = 0; i < n; i++) {
+        pref += a[i];
+        ans += sums[pref - x];
+        sums[pref]++;
+    }
+
+    cout << ans << endl;
+}
+/*
+
+
+*/
